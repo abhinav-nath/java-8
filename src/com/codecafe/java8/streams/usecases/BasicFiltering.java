e7ea1c1c5e26d32a23e5e5a2b35a90a9a3e060f0
Abhinav Nath <abhinavnath@ymail.com>
2020-04-20 03:00:48 +0530

    added use cases of streams

---
+++ src/com/codecafe/java8/streams/usecases/BasicFiltering.java
@@ -0,0 +1,20 @@
+package com.codecafe.java8.streams.usecases;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BasicFiltering {
+
+	public static void main(String[] args) {
+
+		List<String> strings = Arrays.asList("ABC", "DEF", "GH", "IJKL", "MN", "OPQ");
+		
+		// filter strings with more than 2 characters
+		System.out.println(strings.stream()
+				                  .filter(s -> s.length() > 2)
+				                  .collect(Collectors.toList()));
+
+	}
+
+}
---
+++ src/com/codecafe/java8/streams/usecases/SummaryStatistics.java
@@ -0,0 +1,29 @@
+package com.codecafe.java8.streams.usecases;
+
+import java.util.ArrayList;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Random;
+
+public class SummaryStatistics {
+
+	public static void main(String[] args) {
+		
+		List<Integer> randomNumbers = new ArrayList<>();
+		
+		for(int i=0; i<10; i++)
+			randomNumbers.add(i, new Random().nextInt(100));
+		
+		System.out.println("list of random numbers :\n" + randomNumbers);
+		
+		IntSummaryStatistics stats = randomNumbers.stream()
+				                                  .mapToInt((x) -> x)
+				                                  .summaryStatistics();
+		
+		System.out.println("\nmaximum number in the list : " + stats.getMax());
+		System.out.println("minimum number in the list : " + stats.getMin());
+		System.out.println("sum of all numbers in the list : " + stats.getSum());
+		System.out.println("average of numbers in the list : " + stats.getAverage());
+	}
+
+}
