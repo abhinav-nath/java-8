b6dac8997ac9585df82d4dcd1e381baa4e0e769f
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2020-04-27 05:26:17 +0530

    test commit

--- src/com/codecafe/java8/streams/usecases/ReadFromFile.java
+++ src/com/codecafe/java8/streams/usecases/ReadFromFile.java
@@ -7,15 +7,15 @@ import java.nio.file.Paths;
 // Read a file and count the occurrence of a word in it
 public class ReadFromFile {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		String path = "C:\\Users\\Abhinav\\git\\java-8\\src\\com\\codecafe\\java8\\streams\\usecases\\hosts";
+        String path = "C:\\Users\\Abhinav\\git\\java-8\\src\\com\\codecafe\\java8\\streams\\usecases\\hosts";
 
-		String searchWord = "localhost";
+        String searchWord = "localhost";
 
         System.out.printf("occurence of word [%s] = %d", searchWord, Files.lines(Paths.get(path))
                                                                           .filter(line -> line.contains(searchWord))
                                                                           .count());
-	}
+    }
 
 }
