1c9951f2ffaaa1ea2c195243217f116c563b06d9
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-06-28 23:36:38 +0530

    test commit

--- src/com/codecafe/java8/streams/usecases/PrimeNumber.java
+++ src/com/codecafe/java8/streams/usecases/PrimeNumber.java
@@ -6,16 +6,14 @@ public class PrimeNumber {
 
     public static void main(String[] args) {
 
-        for(int i=1; i<=100; i++)
+        for (int i = 1; i <= 100; i++)
             System.out.printf("isPrime(%d)? %b\n", i, isPrime(i));
 
     }
 
     private static boolean isPrime(int number) {
 
-        return number > 1 &&
-                IntStream.range(2, number)
-                         .noneMatch(i -> number % i == 0);
+        return number > 1 && IntStream.range(2, number).noneMatch(i -> number % i == 0);
     }
 
 }
